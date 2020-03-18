97894bd6730119e0d7b1d5e34aec74b673ddac5b
testuser-pleaseignore <test_user_please_ignore@hotmail.com>
2020-03-18 16:39:41 +0700

    ฝึกเล่นvectorแปป

---
+++ menu_cop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct bread{
+    string name;// name = ชื่อหนมปัง    
+    int  stock , cost , day ,sold ; /*
+        stock = จน ที่ผลิต
+        cost = ราคา่
+        day = ผลิตมาแล้วกี่วัน
+        sold = ขายไปแล้วเท่าไหร่
+    */
+};
+
+int main(){
+cout << "MENU:"<<setw(30)<<"sell dai :"<<setw(15)<<"InStock :"<<setw(15)<<"amount :"<<endl;
+
+vector<bread> listgoods;
+listgoods.push_back ()
+//ขอลองฝึกเล่นvectorแปป
+    return 0;
+}
