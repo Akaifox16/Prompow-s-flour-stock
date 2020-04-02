7a20beddafc2d25856df115ac74535d5dc6307ac
ProtonNote <naruson_k@cmu.ac.th>
2020-04-02 23:55:10 +0700

    edit

--- Project1/chart.h
+++ Project1/chart.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "bread.h"
 
 namespace Project1 {
 
@@ -38,12 +39,15 @@ namespace Project1 {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::DateTimePicker^ dateTimePicker1;
+
+
+
+
 	private: System::Windows::Forms::Label^ label5;
-	private: System::Windows::Forms::TextBox^ textBox3;
+
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::TextBox^ textBox2;
+
 
 	protected:
 
@@ -64,12 +68,9 @@ namespace Project1 {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->dateTimePicker1 = (gcnew System::Windows::Forms::DateTimePicker());
 			this->label5 = (gcnew System::Windows::Forms::Label());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -79,7 +80,7 @@ namespace Project1 {
 				static_cast<System::Byte>(0)));
 			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(62)), static_cast<System::Int32>(static_cast<System::Byte>(120)),
 				static_cast<System::Int32>(static_cast<System::Byte>(138)));
-			this->label1->Location = System::Drawing::Point(234, 19);
+			this->label1->Location = System::Drawing::Point(252, 18);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(87, 32);
 			this->label1->TabIndex = 0;
@@ -91,11 +92,11 @@ namespace Project1 {
 			this->button1->BackColor = System::Drawing::Color::PaleTurquoise;
 			this->button1->FlatAppearance->BorderSize = 0;
 			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button1->Location = System::Drawing::Point(240, 224);
+			this->button1->Location = System::Drawing::Point(321, 168);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(84, 32);
 			this->button1->TabIndex = 1;
-			this->button1->Text = L"submit";
+			this->button1->Text = L"Close";
 			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &chart::button1_Click);
 			// 
@@ -106,11 +107,12 @@ namespace Project1 {
 				static_cast<System::Byte>(0)));
 			this->label2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(62)), static_cast<System::Int32>(static_cast<System::Byte>(120)),
 				static_cast<System::Int32>(static_cast<System::Byte>(138)));
-			this->label2->Location = System::Drawing::Point(77, 70);
+			this->label2->Location = System::Drawing::Point(68, 88);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(67, 23);
 			this->label2->TabIndex = 2;
 			this->label2->Text = L"Name";
+			this->label2->Click += gcnew System::EventHandler(this, &chart::label2_Click);
 			// 
 			// textBox1
 			// 
@@ -119,58 +121,12 @@ namespace Project1 {
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Century Gothic", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->textBox1->ForeColor = System::Drawing::SystemColors::Window;
-			this->textBox1->Location = System::Drawing::Point(163, 74);
+			this->textBox1->Location = System::Drawing::Point(169, 93);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(100, 21);
 			this->textBox1->TabIndex = 3;
 			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Century Gothic", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label3->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(62)), static_cast<System::Int32>(static_cast<System::Byte>(120)),
-				static_cast<System::Int32>(static_cast<System::Byte>(138)));
-			this->label3->Location = System::Drawing::Point(309, 68);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(56, 23);
-			this->label3->TabIndex = 4;
-			this->label3->Text = L"Price";
-			// 
-			// textBox2
-			// 
-			this->textBox2->BackColor = System::Drawing::SystemColors::WindowFrame;
-			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->textBox2->Font = (gcnew System::Drawing::Font(L"Century Gothic", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->textBox2->ForeColor = System::Drawing::SystemColors::Window;
-			this->textBox2->Location = System::Drawing::Point(384, 73);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 21);
-			this->textBox2->TabIndex = 5;
-			this->textBox2->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
-			// 
-			// label4
-			// 
-			this->label4->AutoSize = true;
-			this->label4->Font = (gcnew System::Drawing::Font(L"Century Gothic", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label4->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(62)), static_cast<System::Int32>(static_cast<System::Byte>(120)),
-				static_cast<System::Int32>(static_cast<System::Byte>(138)));
-			this->label4->Location = System::Drawing::Point(181, 174);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(54, 23);
-			this->label4->TabIndex = 6;
-			this->label4->Text = L"Date";
-			// 
-			// dateTimePicker1
-			// 
-			this->dateTimePicker1->CalendarMonthBackground = System::Drawing::SystemColors::WindowFrame;
-			this->dateTimePicker1->Location = System::Drawing::Point(254, 177);
-			this->dateTimePicker1->Name = L"dateTimePicker1";
-			this->dateTimePicker1->Size = System::Drawing::Size(154, 20);
-			this->dateTimePicker1->TabIndex = 8;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &chart::textBox1_TextChanged);
 			// 
 			// label5
 			// 
@@ -179,24 +135,38 @@ namespace Project1 {
 				static_cast<System::Byte>(0)));
 			this->label5->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(62)), static_cast<System::Int32>(static_cast<System::Byte>(120)),
 				static_cast<System::Int32>(static_cast<System::Byte>(138)));
-			this->label5->Location = System::Drawing::Point(181, 124);
+			this->label5->Location = System::Drawing::Point(299, 93);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(82, 23);
 			this->label5->TabIndex = 9;
 			this->label5->Text = L"Amount";
 			// 
-			// textBox3
+			// button2
 			// 
-			this->textBox3->BackColor = System::Drawing::SystemColors::WindowFrame;
-			this->textBox3->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->textBox3->Font = (gcnew System::Drawing::Font(L"Century Gothic", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->button2->BackColor = System::Drawing::Color::PaleTurquoise;
+			this->button2->FlatAppearance->BorderSize = 0;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Location = System::Drawing::Point(185, 168);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(84, 32);
+			this->button2->TabIndex = 11;
+			this->button2->Text = L"Add chart";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &chart::button2_Click);
+			// 
+			// textBox2
+			// 
+			this->textBox2->BackColor = System::Drawing::SystemColors::WindowFrame;
+			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Century Gothic", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox3->ForeColor = System::Drawing::SystemColors::Window;
-			this->textBox3->Location = System::Drawing::Point(280, 124);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(100, 21);
-			this->textBox3->TabIndex = 10;
-			this->textBox3->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->textBox2->ForeColor = System::Drawing::SystemColors::Window;
+			this->textBox2->Location = System::Drawing::Point(387, 95);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 21);
+			this->textBox2->TabIndex = 12;
+			this->textBox2->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &chart::textBox2_TextChanged_1);
 			// 
 			// chart
 			// 
@@ -204,13 +174,10 @@ namespace Project1 {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(34)), static_cast<System::Int32>(static_cast<System::Byte>(39)),
 				static_cast<System::Int32>(static_cast<System::Byte>(51)));
-			this->ClientSize = System::Drawing::Size(587, 280);
-			this->Controls->Add(this->textBox3);
-			this->Controls->Add(this->label5);
-			this->Controls->Add(this->dateTimePicker1);
-			this->Controls->Add(this->label4);
+			this->ClientSize = System::Drawing::Size(585, 260);
 			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->label3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label5);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->button1);
@@ -218,6 +185,7 @@ namespace Project1 {
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"chart";
 			this->Text = L"chart";
+			this->Load += gcnew System::EventHandler(this, &chart::chart_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -229,6 +197,72 @@ namespace Project1 {
 	}
 private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 	this->Hide();
+	
+}
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	
+}
+private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	
+}
+private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	
+}
+private: System::Void dateTimePicker1_ValueChanged(System::Object^ sender, System::EventArgs^ e) {
+
+}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	String^ name = textBox1->Text;
+	String^ sold = textBox2->Text;
+
+	// String^ name to string namefos
+	char cStr[50] = { 0 };
+	String^ clrString = name;
+	if (clrString->Length < sizeof(cStr))
+		sprintf(cStr, "%s", clrString);
+	std::string nameofs = cStr;
+
+	// String^ sold to string soldfos to int soldint
+	std::ofstream file("today_baked.txt");
+	char cStr[50] = { 0 };
+	String^ clrString = sold;
+	if (clrString->Length < sizeof(cStr))
+		sprintf(cStr, "%s", clrString);
+	std::string soldofs = cStr;
+	int soldint = std::stoi(soldofs);
+
+
+	std::ifstream file("today_baked.txt");
+	std::string textline;
+	std::vector<bread> listed;
+	while (std::getline(file, textline)) {
+		bread tmp;
+		std::istringstream iss(textline);
+		iss >> tmp.name >> tmp.stock >> tmp.cost >> tmp.sold >> tmp.day;
+		listed.push_back(tmp);
+	}
+	file.close();
+	std::ofstream out("today_baked.txt");
+	for (int j = 0; j < listed.size(); j++) {
+		if (nameofs == listed[j].name) {
+			listed[j].sold = soldint;
+			listed[j].amount -= soldint;
+		}
+	}
+	for (int i = 0; i < listed.size(); i++) {
+		std::ostringstream oss;
+		oss << listed.at(i).name << " " << listed.at(i).stock << " " << listed.at(i).cost << " " << listed.at(i).sold << " " << listed.at(i).day;
+		textline = oss.str();
+		out << textline << '\n';
+	}
+	out.close();
+}
+private: System::Void comboBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+
+}
+private: System::Void textBox2_TextChanged_1(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void chart_Load(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
