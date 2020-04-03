274e223978c9944ab25e58339d520f8e3b0323af
Akaifox16 <kamonpat_s@cmu.ac.th>
2020-04-04 00:27:14 +0700

    Revert "Dashboard กับ Add chart ใช้ได้แล้ว ปริ่มมมมมมม"
    This reverts commit 4459f1acc83a1be314efb4b50706ec9292817242.

--- Project1/chart.h
+++ Project1/chart.h
@@ -48,11 +48,9 @@ namespace Project1 {
 	private: System::Windows::Forms::Label^ label5;
 
 	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
 
-
-
+	private: Bunifu::Framework::UI::BunifuMaterialTextbox^ bunifuMaterialTextbox1;
+	private: Bunifu::Framework::UI::BunifuMaterialTextbox^ bunifuMaterialTextbox2;
 
 
 	protected:
@@ -75,8 +73,8 @@ namespace Project1 {
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->bunifuMaterialTextbox1 = (gcnew Bunifu::Framework::UI::BunifuMaterialTextbox());
+			this->bunifuMaterialTextbox2 = (gcnew Bunifu::Framework::UI::BunifuMaterialTextbox());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -85,10 +83,9 @@ namespace Project1 {
 			this->label1->Font = (gcnew System::Drawing::Font(L"Century Gothic", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label1->ForeColor = System::Drawing::Color::White;
-			this->label1->Location = System::Drawing::Point(336, 22);
-			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label1->Location = System::Drawing::Point(252, 18);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(107, 40);
+			this->label1->Size = System::Drawing::Size(87, 32);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Chart";
 			this->label1->Click += gcnew System::EventHandler(this, &chart::label1_Click);
@@ -100,10 +97,9 @@ namespace Project1 {
 			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->button1->Font = (gcnew System::Drawing::Font(L"Century Gothic", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(428, 207);
-			this->button1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button1->Location = System::Drawing::Point(321, 168);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(112, 39);
+			this->button1->Size = System::Drawing::Size(84, 32);
 			this->button1->TabIndex = 1;
 			this->button1->Text = L"Close";
 			this->button1->UseVisualStyleBackColor = false;
@@ -115,10 +111,9 @@ namespace Project1 {
 			this->label2->Font = (gcnew System::Drawing::Font(L"Century Gothic", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label2->ForeColor = System::Drawing::Color::White;
-			this->label2->Location = System::Drawing::Point(91, 108);
-			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label2->Location = System::Drawing::Point(68, 88);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(84, 28);
+			this->label2->Size = System::Drawing::Size(67, 23);
 			this->label2->TabIndex = 2;
 			this->label2->Text = L"Name";
 			// 
@@ -128,10 +123,9 @@ namespace Project1 {
 			this->label5->Font = (gcnew System::Drawing::Font(L"Century Gothic", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label5->ForeColor = System::Drawing::Color::White;
-			this->label5->Location = System::Drawing::Point(399, 114);
-			this->label5->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label5->Location = System::Drawing::Point(299, 93);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(103, 28);
+			this->label5->Size = System::Drawing::Size(82, 23);
 			this->label5->TabIndex = 9;
 			this->label5->Text = L"Amount";
 			// 
@@ -142,45 +136,80 @@ namespace Project1 {
 			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->button2->Font = (gcnew System::Drawing::Font(L"Century Gothic", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(247, 207);
-			this->button2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button2->Location = System::Drawing::Point(185, 168);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(112, 39);
+			this->button2->Size = System::Drawing::Size(84, 32);
 			this->button2->TabIndex = 11;
 			this->button2->Text = L"Add";
 			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &chart::button2_Click);
 			// 
-			// textBox1
+			// bunifuMaterialTextbox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(214, 115);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 22);
-			this->textBox1->TabIndex = 12;
+			this->bunifuMaterialTextbox1->AutoCompleteMode = System::Windows::Forms::AutoCompleteMode::None;
+			this->bunifuMaterialTextbox1->AutoCompleteSource = System::Windows::Forms::AutoCompleteSource::None;
+			this->bunifuMaterialTextbox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->bunifuMaterialTextbox1->characterCasing = System::Windows::Forms::CharacterCasing::Normal;
+			this->bunifuMaterialTextbox1->Cursor = System::Windows::Forms::Cursors::IBeam;
+			this->bunifuMaterialTextbox1->Font = (gcnew System::Drawing::Font(L"Century Gothic", 9.75F));
+			this->bunifuMaterialTextbox1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->bunifuMaterialTextbox1->HintForeColor = System::Drawing::Color::Empty;
+			this->bunifuMaterialTextbox1->HintText = L"";
+			this->bunifuMaterialTextbox1->isPassword = false;
+			this->bunifuMaterialTextbox1->LineFocusedColor = System::Drawing::Color::Blue;
+			this->bunifuMaterialTextbox1->LineIdleColor = System::Drawing::Color::Gray;
+			this->bunifuMaterialTextbox1->LineMouseHoverColor = System::Drawing::Color::Blue;
+			this->bunifuMaterialTextbox1->LineThickness = 3;
+			this->bunifuMaterialTextbox1->Location = System::Drawing::Point(156, 83);
+			this->bunifuMaterialTextbox1->Margin = System::Windows::Forms::Padding(4);
+			this->bunifuMaterialTextbox1->MaxLength = 32767;
+			this->bunifuMaterialTextbox1->Name = L"bunifuMaterialTextbox1";
+			this->bunifuMaterialTextbox1->Size = System::Drawing::Size(113, 33);
+			this->bunifuMaterialTextbox1->TabIndex = 13;
+			this->bunifuMaterialTextbox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Left;
+			this->bunifuMaterialTextbox1->OnValueChanged += gcnew System::EventHandler(this, &chart::bunifuMaterialTextbox1_OnValueChanged);
 			// 
-			// textBox2
+			// bunifuMaterialTextbox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(551, 120);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 22);
-			this->textBox2->TabIndex = 13;
+			this->bunifuMaterialTextbox2->AutoCompleteMode = System::Windows::Forms::AutoCompleteMode::None;
+			this->bunifuMaterialTextbox2->AutoCompleteSource = System::Windows::Forms::AutoCompleteSource::None;
+			this->bunifuMaterialTextbox2->characterCasing = System::Windows::Forms::CharacterCasing::Normal;
+			this->bunifuMaterialTextbox2->Cursor = System::Windows::Forms::Cursors::IBeam;
+			this->bunifuMaterialTextbox2->Font = (gcnew System::Drawing::Font(L"Century Gothic", 9.75F));
+			this->bunifuMaterialTextbox2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->bunifuMaterialTextbox2->HintForeColor = System::Drawing::Color::Empty;
+			this->bunifuMaterialTextbox2->HintText = L"";
+			this->bunifuMaterialTextbox2->isPassword = false;
+			this->bunifuMaterialTextbox2->LineFocusedColor = System::Drawing::Color::Blue;
+			this->bunifuMaterialTextbox2->LineIdleColor = System::Drawing::Color::Gray;
+			this->bunifuMaterialTextbox2->LineMouseHoverColor = System::Drawing::Color::Blue;
+			this->bunifuMaterialTextbox2->LineThickness = 3;
+			this->bunifuMaterialTextbox2->Location = System::Drawing::Point(403, 88);
+			this->bunifuMaterialTextbox2->Margin = System::Windows::Forms::Padding(4);
+			this->bunifuMaterialTextbox2->MaxLength = 32767;
+			this->bunifuMaterialTextbox2->Name = L"bunifuMaterialTextbox2";
+			this->bunifuMaterialTextbox2->Size = System::Drawing::Size(113, 33);
+			this->bunifuMaterialTextbox2->TabIndex = 14;
+			this->bunifuMaterialTextbox2->TextAlign = System::Windows::Forms::HorizontalAlignment::Left;
+			this->bunifuMaterialTextbox2->OnValueChanged += gcnew System::EventHandler(this, &chart::bunifuMaterialTextbox2_OnValueChanged);
 			// 
 			// chart
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(34)), static_cast<System::Int32>(static_cast<System::Byte>(39)),
 				static_cast<System::Int32>(static_cast<System::Byte>(51)));
-			this->ClientSize = System::Drawing::Size(780, 320);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
+			this->ClientSize = System::Drawing::Size(585, 260);
+			this->Controls->Add(this->bunifuMaterialTextbox2);
+			this->Controls->Add(this->bunifuMaterialTextbox1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
-			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"chart";
 			this->Text = L"chart";
 			this->Load += gcnew System::EventHandler(this, &chart::chart_Load);
@@ -198,18 +227,22 @@ private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e
 	
 }
 private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-	String^ name = textBox1->Text;
-	String^ sold = textBox2->Text;
-	textBox1->Clear();
-	textBox2->Clear();
+	String^ name = bunifuMaterialTextbox1->Text;
+	String^ sold = bunifuMaterialTextbox2->Text;
 
 	// String^ name to string namefos
+	char cStr[50] = { 0 };
 	String^ clrString = name;
-	std::string nameofs = marshal_as<std::string>(clrString);
+	if (clrString->Length < sizeof(cStr))
+		sprintf(cStr, "%s", clrString);
+	std::string nameofs = cStr;
 
 	// String^ sold to string soldfos to int soldint
+	cStr[50] = { 0 };
 	clrString = sold;
-	std::string soldofs = marshal_as<std::string>(clrString);
+	if (clrString->Length < sizeof(cStr))
+		sprintf(cStr, "%s", clrString);
+	std::string soldofs = cStr;
 	int soldint = std::stoi(soldofs);
 
 
@@ -223,7 +256,7 @@ private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e
 		listed.push_back(tmp);
 	}
 	in.close();
-	StreamWriter^ sw = gcnew StreamWriter("today_baked.txt");
+	StreamWriter^ sw = gcnew StreamWriter("file.txt");
 	for (int j = 0; j < listed.size(); j++) {
 		if (nameofs == listed[j].name) {
 			listed[j].sold = soldint;
@@ -232,9 +265,9 @@ private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e
 	}
 	for (int i = 0; i < listed.size(); i++) {
 		std::ostringstream oss;
-		oss << listed.at(i).name << " " << listed.at(i).stock << " " << listed.at(i).cost << " " << listed.at(i).sold << " " << listed.at(i).day << "\n";
+		oss << listed.at(i).name << " " << listed.at(i).stock << " " << listed.at(i).cost << " " << listed.at(i).sold << " " << listed.at(i).day;
 		String^ str = marshal_as<String^>(oss.str());
-		sw->Write(str );
+		sw->Write(str + "/n");
 	}
 	sw->Close();
 }
